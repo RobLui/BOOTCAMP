62da8d774e2bc115d671b351e1d1b1ff86cd217b
andrerobbe <mauritsrobbe@hotmail.com>
2017-03-08 16:35:34 +0100

    preload clean-up

--- www/js/levels/_preload.js
+++ www/js/levels/_preload.js
@@ -1,24 +1,35 @@
 var PreloadState = {
     preload: function() {
+        game.time.advancedTiming.enable = true;
+        //MENU
+        game.load.image('logo', 'assets/logo.png');
+        game.load.image('instructions', 'assets/instructions.png');
+        game.load.image('play','assets/play.png');
+        game.load.image('back','assets/back.png');
+        game.load.image('what','assets/what.png');
+        
+        //OBJECTS
+        game.load.image('bg', 'assets/bg.jpg');
         game.load.image('tileset','assets/tileset.png');
         game.load.image("bal", "assets/bal.png");
         game.load.image('hole','assets/hole.png');
-        game.load.image('logo', 'assets/logo.png');
-        game.load.image('instructions', 'assets/instructions.png');
-        game.load.image('credits', 'assets/credits.png');
-        game.load.image('bg', 'assets/bg.jpg');
-        game.load.image('play','assets/play.png');
-        game.load.image('back','assets/back.png');
-        game.load.image('arrow','assets/arrow.png');
-        game.load.image('what','assets/what.png');
-        game.load.image('menuBtn','assets/menu_btn.png');
+        game.load.image('arrow','assets/arrow.png');    //UNUSED
         game.load.image('winningHole','assets/winningHole.png');
         game.load.image('enemy','assets/enemy.png');
         game.load.spritesheet("harts","assets/life.png",180,50,4);
-        game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
-        game.load.tilemap('level2', 'assets/level2.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.spritesheet("laser","assets/laser.png",50,20,2);
-        game.time.advancedTiming.enable = true;
+        
+        //CREDITS
+        game.load.image('credits', 'assets/credits.png');
+        game.load.image('menuBtn','assets/menu_btn.png');
+        
+        //LEVEL 1
+        game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('intro_level2', 'assets/intro_level2.png');
+        
+        //LEVEL 2
+        game.load.tilemap('level2', 'assets/level2.json', null, Phaser.Tilemap.TILED_JSON);
+        
     },
     create: function() {
         game.state.start("menu");
